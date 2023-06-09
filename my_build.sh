eba38a7062dae6e0fd0d91ed3320b6f4248b2088
salaxieb <salaxieb.ildar@gmail.com>
2023-06-09 20:19:01 +0100

    make signle build command

---
+++ my_build.sh
@@ -0,0 +1,64 @@
+sudo apt-get update # optional but recommended
+sudo apt-get -y upgrade # optional but recommended
+# sudo apt-get -y install nvidia-driver-525
+
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+#add public keys
+
+# Old key
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+
+# new key, added 2022-04-25 22:52
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+
+
+sudo apt update
+sudo apt install cuda-toolkit-11-6
+
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+export CUDA_HOME=/usr/local/cuda
+export PATH="/usr/local/cuda/bin:$PATH"
+
+
+# PyTorch
+pip install torch==1.13.1 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torchmetrics==0.11.1
+# fbank
+pip install librosa==0.8.1
+
+# phonemizer pypinyin
+apt-get install espeak-ng
+## OSX: brew install espeak
+pip install phonemizer==3.2.1 pypinyin==0.48.0
+
+# lhotse update to newest version
+# https://github.com/lhotse-speech/lhotse/pull/956
+# https://github.com/lhotse-speech/lhotse/pull/960
+pip uninstall lhotse
+pip uninstall lhotse
+pip install git+https://github.com/lhotse-speech/lhotse
+
+# k2
+# find the right version in https://huggingface.co/csukuangfj/k2
+pip install https://huggingface.co/csukuangfj/k2/resolve/main/cuda/k2-1.23.4.dev20230224+cuda11.6.torch1.13.1-cp310-cp310-linux_x86_64.whl
+
+# icefall
+git clone https://github.com/k2-fsa/icefall
+cd icefall
+pip install -r requirements.txt
+export PYTHONPATH=`pwd`/../icefall:$PYTHONPATH
+echo "export PYTHONPATH=`pwd`/../icefall:\$PYTHONPATH" >> ~/.zshrc
+echo "export PYTHONPATH=`pwd`/../icefall:\$PYTHONPATH" >> ~/.bashrc
+cd -
+source ~/.zshrc
+
+# valle
+git clone https://github.com/lifeiteng/valle.git
+cd valle
+pip install -e .
